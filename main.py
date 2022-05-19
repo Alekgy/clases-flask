8a5d44a4456b7ea939665d63e3f2862c2ef8fcbf
alejandro <alekgy@gmail.com>
2022-05-19 09:57:21 -0500

    add class of flask

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
-from flask import Flask, redirect, request, make_response, render_template
+from crypt import methods
+from ensurepip import bootstrap
+from flask import redirect, request, make_response, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app =  Flask(__name__)
+app = create_app()
+
+todos = ['Comprar Cafe', 'Enviar Solicitud De Compra', 'Entregar Video Al Productor']
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+        'login_form': login_form,
+        'username': username,
+    }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session ['username'] = username
+
+        flash('Nombre de usuario registrado con éxito')
+
+        return redirect(url_for('index'))
     
-    return render_template('hello.html', user_ip=user_ip)
+    return render_template('hello.html', **context)
