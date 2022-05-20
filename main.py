6f4cd08d41f3ffee517d545958da2f7a7a9b73c5
alejandro <alekgy@gmail.com>
2022-05-19 19:13:05 -0500

    add moadding modification to code views in classes

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from crypt import methods
-from ensurepip import bootstrap
-from flask import redirect, request, make_response, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+
 from app import create_app
 from app.forms import LoginForm
 
 app = create_app()
 
-todos = ['Comprar Cafe', 'Enviar Solicitud De Compra', 'Entregar Video Al Productor']
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
 
 @app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner().run(tests)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
 
+
 @app.errorhandler(500)
-def internal_server_error(error):
+def server_error(error):
     return render_template('500.html', error=error)
+
 
 @app.route('/')
 def index():
@@ -33,25 +36,15 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form,
-        'username': username,
+        'username': username
     }
 
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session ['username'] = username
-
-        flash('Nombre de usuario registrado con éxito')
-
-        return redirect(url_for('index'))
-    
     return render_template('hello.html', **context)
